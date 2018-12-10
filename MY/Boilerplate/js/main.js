4c6d066900cb278b209bac28e1b1b55257fb2e11
dgladovich <dgladovich@gmail.com>
2018-12-10 09:36:18 +0200

    Creating backbone model

--- MY/Boilerplate/js/main.js
+++ MY/Boilerplate/js/main.js
@@ -2,3 +2,11 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+var Song = Backbone.Model.extend({
+    initialize: function(){
+        console.log("A new sog was been created.")
+    }
+});
+
+var song = new Song();
