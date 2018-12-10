55a1ee485ddbc82f74e0b918204a6c76e90415e5
dgladovich <dgladovich@gmail.com>
2018-12-10 11:06:13 +0200

    Collections working with them

--- MY/Collections/js/main.js
+++ MY/Collections/js/main.js
@@ -9,11 +9,24 @@ var Songs = Backbone.Collection.extend({
 });
 
 var songs = new Songs([
-    new Song({title: "Song 1"}),
-    new Song({title: "Song 2"}),
-    new Song({title: "Song 3"})
+    new Song({title: "Song 1", genre: "Disco", downloads: 110}),
+    new Song({title: "Song 2", genre: "Dancing", downloads: 123}),
+    new Song({title: "Song 3", genre: "Pop", downloads: 210})
 ])
-songs.add(new Song({title: "Song 4"}));
+songs.add(new Song({title: "Song 4", genre: "Jazz", downloads: 10}), {at:0}); // Insert on first place in the collection
+songs.push(new Song({title: "Song 5", genre: "Jazz", downloads: 12})); // Insert on last place in the collection
+let songsJazz = songs.where({genre: "Jazz"});
+console.log(songsJazz);
+
+
+let filteredSongs = songs.filter(function(song){ //Filtering Method
+    return song.get("downloads") > 100
+})
+console.log('Filtered songs', filteredSongs);
+
+songs.each(function(song){ //Collection each method
+    console.log(song);
+})
 
 let firstSong = songs.at(0);
 console.log(firstSong);
