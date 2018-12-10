c162313576e2ec19be19da2b4144f9a46a866b8e
dgladovich <dgladovich@gmail.com>
2018-12-10 09:46:06 +0200

    Working with Model attributes

--- MY/Boilerplate/js/main.js
+++ MY/Boilerplate/js/main.js
@@ -6,7 +6,18 @@
 var Song = Backbone.Model.extend({
     initialize: function(){
         console.log("A new sog was been created.")
+    },
+    defaults:{
+        genre: "Jazz"
     }
 });
 
-var song = new Song();
+var song = new Song({
+    artist: "Artist",
+    publishYear: 1967
+});
+song.set("title", "Blue in Green");
+
+console.log(song.get("artist"));
+song.set({artist: "TestArtist"})
+console.log(song.get("artist"));
