38888e8c7e4ccc1cf4a1a0523b7929e2e870cd6b
dgladovich <dgladovich@gmail.com>
2018-12-12 16:22:15 +0200

    Add views

--- MY/Views/js/main.js
+++ MY/Views/js/main.js
@@ -2,21 +2,33 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
-
+let Song = Backbone.Model.extend();
+let Songs = Backbone.Collection.extend({
+    model: Song
+})
 let SongView = Backbone.View.extend({
-    tagName: "span",
-    className: "song",
-    id: "1234",
-    attributes:{
-        "data-genre" : "Jazz"
-    },
+    tagName: "li",
     render: function(){
-        this.$el.html('Hello world');
+        this.$el.html(this.model.get('title'));
 
         return this;
     }
 })
+let SongsView = new Backbone.View.extend({
+    render: function(){
+        let self = this;
+        this.model.each(function(song){
+            let songView = new SongView({ model: song });
+            self.$el.append(songView.render().$el);
+        })
+    }
+})
 
-let songView = new SongView();
+let songs = new Songs([
+    new Song({title: "Song title 2"}),
+    new Song({title: "Song title 3"}),
+    new Song({title: "Song title 4"})
+])
 
-$("#container").html(songView.render().$el);
+let songsView = new SongsView({ el: '#songs', model: songs});
+songsView.render();
